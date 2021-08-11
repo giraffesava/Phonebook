4ee7157855069782398ca1db37a7b8142c37536f
Sava <savely54@mail.ru>
2021-08-12 04:06:07 +0700

    added render conditions

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,21 +1,58 @@
 import React from 'react'
 import Person from './../Person/Person'
+import classes from './Users.module.css'
 
-const Users = ({ searchName, data2, onClick }) => {
-  const func = () => {
+const Users = ({ searchName, users, onClick, checkSort }) => {
+  const usersData = () => {
+    // If have searchName from input component then do search
     if (searchName) {
-      return data2.map((item) => {
-        if (item.name.toLowerCase().includes(searchName.toLowerCase())) {
-          return <Person key={item.id} information={item} onClick={onClick} />
+      return users.map((person) => {
+        if (person.name.toLowerCase().includes(searchName.toLowerCase())) {
+          return (
+            <Person key={person.id} information={person} onClick={onClick} />
+          )
         }
       })
-    } else {
-      return data2.map((item) => (
-        <Person key={item.id} information={item} onClick={onClick} />
+    }
+
+    // If user choose "sort by group" then do groupping
+    else if (checkSort === 'grouped') {
+      const grouped = users.reduce((group, person) => {
+        const letter = person.name.charAt(0).toUpperCase()
+        if (!group[letter]) {
+          group[letter] = []
+        }
+        group[letter].push(person)
+        return group
+      }, {})
+      return Object.keys(grouped).map((letter) => (
+        <>
+          <h1 className={classes.groupedLetter}>{letter}</h1>
+          {grouped[letter].map((person) => (
+            <Person key={person.id} information={person} onClick={onClick} />
+          ))}
+        </>
+      ))
+    }
+
+    // If user choose "sort by name" do sorting
+    else if (checkSort === 'name') {
+      const sorted = [...users].sort((a, b) => {
+        return a.name.localeCompare(b.name)
+      })
+      return sorted.map((person) => (
+        <Person key={person.id} information={person} onClick={onClick} />
+      ))
+    }
+
+    // All other cases
+    else {
+      return users.map((person) => (
+        <Person key={person.id} information={person} onClick={onClick} />
       ))
     }
   }
-  return func()
+  return usersData()
 }
 
 export default Users
