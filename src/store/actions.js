1688b0d33cb25150ec8aaae13e5242a11cfb83bd
Sava <savely54@mail.ru>
2021-08-13 00:19:27 +0700

    added a new action

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,11 +2,19 @@ export const Types = {
   SEND_REQUEST_CONTACTS: 'SEND_REQUEST',
   GET_CONTACTS: 'GET_CONTACTS',
   FAILED_CONTACTS: 'FAILED_CONTACTS',
+  CHANGE_USERS: 'CHANGE_USERS',
 }
 
 export const sendRequestContacts = () => {
   return {
     type: Types.SEND_REQUEST_CONTACTS,
+  }
+}
+
+export const changeUsers = (newUser) => {
+  return {
+    type: Types.CHANGE_USERS,
+    newUser,
   }
 }
 
