57fb72798e9a344c0b2200294950a37fef8c3589
Sava <savely54@mail.ru>
2021-08-14 12:01:29 +0700

    did code more lean and readable

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,14 +4,17 @@ import classes from './Users.module.css'
 import { v4 as uuidv4 } from 'uuid'
 
 const Users = ({ searchName, users, onClick, checkSort }) => {
+  //Add function to create <Person /> component and do code more lean
+  const personComponentCreator = (person) => {
+    return <Person key={person.id} information={person} onClick={onClick} />
+  }
+
   const usersData = () => {
     // If have searchName from input component then do search
     if (searchName && users) {
       return users.map((person) => {
         if (person.name.toLowerCase().includes(searchName.toLowerCase())) {
-          return (
-            <Person key={person.id} information={person} onClick={onClick} />
-          )
+          return personComponentCreator(person)
         }
       })
     }
@@ -31,9 +34,7 @@ const Users = ({ searchName, users, onClick, checkSort }) => {
       return Object.keys(grouped).map((letter) => (
         <div key={uuidv4()}>
           <h1 className={classes.groupedLetter}>{letter}</h1>
-          {grouped[letter].map((person) => (
-            <Person key={person.id} information={person} onClick={onClick} />
-          ))}
+          {grouped[letter].map((person) => personComponentCreator(person))}
         </div>
       ))
     }
@@ -43,16 +44,12 @@ const Users = ({ searchName, users, onClick, checkSort }) => {
       const sorted = [...users].sort((a, b) => {
         return a.name.localeCompare(b.name)
       })
-      return sorted.map((person) => (
-        <Person key={person.id} information={person} onClick={onClick} />
-      ))
+      return sorted.map((person) => personComponentCreator(person))
     }
 
     // All other cases
     else {
-      return users.map((person) => (
-        <Person key={person.id} information={person} onClick={onClick} />
-      ))
+      return users.map((person) => personComponentCreator(person))
     }
   }
   return usersData()
